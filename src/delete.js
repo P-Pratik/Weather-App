196898065a9998d6cfc406253ef1a740d15a8e65
P-Pratik <114237737+P-Pratik@users.noreply.github.com>
2023-10-27 01:27:33 +0530

    small

---
+++ src/delete.js
@@ -0,0 +1,30 @@
+let name = "Pune,India";
+
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "Weather",
+    "database": "MiniPro",
+    "dataSource": "Cluster0",
+    "filter": {
+        "name" : name
+    },
+});
+
+var config = {
+    method: 'post',
+    url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-clape/endpoint/data/v1/action/deleteOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'QZcQn6106uI6SjhuQgvYmRSsQS6lSm48rdkLBXbTAGUIg0k7CSFTErab9QjmbwF8',
+    },
+    data: data
+};
+            
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
